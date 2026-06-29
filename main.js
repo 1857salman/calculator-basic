8a3a89cf86d6196f8e0539611d1028b655e0e68f
Salman2004 <137329744+1857salman@users.noreply.github.com>
2026-06-29 12:07:14 +0530

    Add html buttons with click events
    html buttons with digits and one addition
    operation. On click button's text content is
    displayed in input text box.

--- main.js
+++ main.js
@@ -1,10 +1,41 @@
-let a = 2,
-  b = 3;
-console.log(add(a, b));
-console.log(divide(a, b));
+let varFirst = "";
+let operator = "";
+let varSecond = "";
 
-let operator = "/";
-console.log(operate(operator, a, b));
+const inputDisplay = document.querySelector("input");
+
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    if (button.parentElement.className === "digits") {
+      inputDisplay.value += button.textContent;
+      console.log(button.textContent);
+      console.log(button.parentElement.className);
+    } else if (button.parentElement.className === "operators") {
+      varFirst = inputDisplay.value;
+      operator = button.textContent;
+
+      console.log("varFirst", varFirst);
+      console.log("operator", operator);
+
+      inputDisplay.value += button.textContent;
+    }
+  });
+});
+
+// This piece of code works fine! tested and adpated above
+// ---------------------------------------------------------
+// const opButton = document.querySelector(".operators button");
+// opButton.addEventListener("click", () => {
+//   inputDisplay.value = opButton.textContent;
+// });
+// console.log(opButton.parentElement);
+// let operator = opButton.textContent;
+// let a = 4.0,
+//   b = 2.0;
+// console.log(operate(operator, a, b));
+// --------------------------------------------------------
 
 function operate(operation, a, b) {
   switch (operation) {
